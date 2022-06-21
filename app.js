7b58882d7b07dc680fd928d5d00079e372cec87f
do-Va <varisdogukan@outlook.com>
2022-06-21 08:01:45 +0300

    Added basic controllers and routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -14,11 +15,13 @@ const errorMiddleware = require('./middleware/error-handler');
 // middleware
 app.use(express.json());
 
-// rootes
+// routes
 
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use('/api/v1/products', productsRouter);
 
 // products route
 
