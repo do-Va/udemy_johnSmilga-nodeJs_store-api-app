1ddd587f8ec8af882cb8e441d599b0597dc5c13b
do-Va <varisdogukan@outlook.com>
2022-06-21 07:46:05 +0300

    Added Initial basic express features.

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 // dotenv dosyasına erişebilmek için ekliyoruz.
 require('dotenv').config();
+
+// async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// rootes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port, console.log(`Server is listenning port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
