755ac53f9bab8f960dd7dd56b9356eec662cc6a1
do-Va <varisdogukan@outlook.com>
2022-06-22 07:39:19 +0300

    Added sort filter

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   // koleksiyonumuzun içerdiği aynı özelliklere sahip queryleri aldıktan sonra queryObject objesine aktarıyoruz. Bu sayede queryler boş gelirse queryObject boş bir obje göndereceği için find koleksiyon içindeki bütün verileri çekecek.
@@ -20,8 +17,21 @@ const getAllProducts = async (req, res) => {
   // $regex ile daha hızlı bir şekilde arama yapabiliriz. 'i' büyük küçük harf duyarlılığı.
   name && (queryObject.name = { $regex: name, $options: 'i' });
 
-  const products = await Product.find(queryObject);
+  // Arama sonucunu result değişkenine atıyoruz.
+  let result = Product.find(queryObject);
 
+  // Eğer gelen queryler içerisinde sort key'i doluysa
+  if (sort) {
+    // birden fazla sort istediğini("name,price") sort metodunun anlayacağı şekilde parçalayıp birleştiriyoruz("name price").
+    const sortList = sort.split(',').join(' ');
+    // ve sort metodunun içerisine gönderip sıralamayı yaptırıyoruz.
+    result = result.sort(sortList);
+  } else {
+    // eğer sort adında bir query yoksa kendi belirlediğimiz bir değere göre sıralatıyoruz.
+    result = result.sort('createdAt');
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
